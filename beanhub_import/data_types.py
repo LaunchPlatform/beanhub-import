98e1bbb6be542315710404f40156e2caf84999be
Fang-Pen Lin <hello@fangpenlin.com>
2024-05-04 22:18:10 -0700

    Add match rules

--- beanhub_import/data_types.py
+++ beanhub_import/data_types.py
@@ -8,8 +8,45 @@ class ImportBaseModel(BaseModel):
     pass
 
 
-class MatchRule(ImportBaseModel):
-    pass
+class StrExactMatch(ImportBaseModel):
+    equals: str
+
+
+class StrContainsMatch(ImportBaseModel):
+    contains: str
+
+
+class StrPrefixMatch(ImportBaseModel):
+    prefix: str
+
+
+class StrSuffixMatch(ImportBaseModel):
+    suffix: str
+
+
+StrMatch = str | StrExactMatch | StrContainsMatch | StrPrefixMatch | StrSuffixMatch
+
+
+class SimpleMatchRule(ImportBaseModel):
+    extractor: StrMatch | None = None
+    file: StrMatch | None = None
+    date: StrMatch | None = None
+    post_date: StrMatch | None = None
+    timezone: StrMatch = None
+    desc: StrMatch = None
+    bank_desc: StrMatch = None
+    currency: StrMatch | None = None
+    category: StrMatch | None = None
+    status: StrMatch | None = None
+    type: StrMatch | None = None
+    source_account: StrMatch | None = None
+    dest_account: StrMatch | None = None
+    note: StrMatch | None = None
+    reference: StrMatch | None = None
+    payee: StrMatch | None = None
+
+
+MatchRule = SimpleMatchRule
 
 
 @enum.unique
@@ -45,6 +82,30 @@ class ActionAddTxn(ImportBaseModel):
 ActionType = ActionAddTxn
 
 
+class SimpleFileMatch(ImportBaseModel):
+    pass
+
+
+class InputConfigDetails(ImportBaseModel):
+    prepend_postings: list[Posting] | None = None
+    appending_postings: list[Posting] | None = None
+
+
+class InputConfig(ImportBaseModel):
+    match: SimpleFileMatch
+    config: InputConfigDetails
+
+
+class OutputConfig(ImportBaseModel):
+    match: SimpleFileMatch
+
+
 class ImportRule(ImportBaseModel):
     match: MatchRule
     actions: list[ActionType]
+
+
+class ImportDoc(ImportBaseModel):
+    input_configs: list[InputConfig]
+    output_configs: list[OutputConfig]
+    import_rules: list[ImportRule]
