86929a217e7845e03b8fe51ea8e106aa7e87feca
Fang-Pen Lin <hello@fangpenlin.com>
2024-05-04 23:09:42 -0700

    Add match

--- beanhub_import/processor.py
+++ beanhub_import/processor.py
@@ -1,8 +1,14 @@
 import os
 import pathlib
+import re
 import typing
 
 from .data_types import ImportDoc
+from .data_types import SimpleFileMatch
+from .data_types import StrContainsMatch
+from .data_types import StrExactMatch
+from .data_types import StrPrefixMatch
+from .data_types import StrSuffixMatch
 
 
 def walk_dir_files(
@@ -13,9 +19,26 @@ def walk_dir_files(
             yield pathlib.Path(root) / file
 
 
+def match_file(
+    pattern: SimpleFileMatch, filepath: pathlib.Path | pathlib.PurePath
+) -> bool:
+    if isinstance(pattern, str):
+        return re.match(pattern, str(filepath)) is not None
+    elif isinstance(pattern, StrExactMatch):
+        return str(filepath) == pattern.equals
+    elif isinstance(pattern, StrContainsMatch):
+        return pattern.contains in str(filepath)
+    elif isinstance(pattern, StrPrefixMatch):
+        return str(filepath).startswith(pattern.prefix)
+    elif isinstance(pattern, StrSuffixMatch):
+        return str(filepath).endswith(pattern.suffix)
+    else:
+        raise ValueError(f"Unexpected pattern type {type(pattern)}")
+
+
 def process_imports(
     import_doc: ImportDoc, input_dir: pathlib.Path, output_dir: pathlib.Path
 ):
-    for input_config in import_doc.input_configs:
-        for root, dirs, files in os.walk(input_dir):
+    for filepath in walk_dir_files(input_dir):
+        for input_config in import_doc.input_configs:
             pass
--- tests/test_processor.py
+++ tests/test_processor.py
@@ -3,6 +3,11 @@ import typing
 
 import pytest
 
+from beanhub_import.processor import match_file
+from beanhub_import.processor import SimpleFileMatch
+from beanhub_import.processor import StrContainsMatch
+from beanhub_import.processor import StrPrefixMatch
+from beanhub_import.processor import StrSuffixMatch
 from beanhub_import.processor import walk_dir_files
 
 
@@ -36,3 +41,25 @@ def test_walk_dir_files(
     assert frozenset(
         p.relative_to(tmp_path) for p in walk_dir_files(tmp_path)
     ) == frozenset(map(pathlib.Path, expected))
+
+
+@pytest.mark.parametrize(
+    "pattern, path, expected",
+    [
+        ("^/path/to/([0-9])+", "/path/to/0", True),
+        ("^/path/to/([0-9])+", "/path/to/0123", True),
+        ("^/path/to/([0-9])+", "/path/to/a0123", False),
+        (Str(contains="foo"), "/path/to/foo", True),
+        (StrContainsMatch(contains="foo"), "/path/to/foobar", True),
+        (StrContainsMatch(contains="foo"), "/path/to/spam-foobar", True),
+        (StrContainsMatch(contains="foo"), "/path/to/spam-fobar", False),
+        (StrPrefixMatch(prefix="foo"), "foo.csv", True),
+        (StrPrefixMatch(prefix="foo"), "foobar.csv", True),
+        (StrPrefixMatch(prefix="foo"), "xfoobar.csv", False),
+        (StrSuffixMatch(suffix="bar"), "foo.csv", True),
+        (StrPrefixMatch(suffix="foo"), "foobar.csv", True),
+        (StrPrefixMatch(prefix="foo"), "xfoobar.csv", False),
+    ],
+)
+def test_match_file(pattern: SimpleFileMatch, path: pathlib.PurePath, expected: bool):
+    assert match_file(pattern, path) == expected
