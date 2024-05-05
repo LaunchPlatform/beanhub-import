4c03ae9266bca9a8fe374e85e3991ddf5d0c5de4
Fang-Pen Lin <hello@fangpenlin.com>
2024-05-05 12:44:59 -0700

    Add post processor

---
+++ beanhub_import/post_processor.py
@@ -0,0 +1,7 @@
+# find out all the txns and their import id
+# find out txns to remove from their original files
+# find out txns to update
+# find out txns to append
+# apply black with filter for remove & update
+# append new txns
+# apply black again
