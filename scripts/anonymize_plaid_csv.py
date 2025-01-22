7f528fe15b029f5e5f207cb255853eec2ac96ddb
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-22 13:46:15 -0800

    Add script for anonymize txn details

---
+++ scripts/anonymize_plaid_csv.py
@@ -0,0 +1,80 @@
+import base64
+import csv
+import datetime
+import decimal
+import random
+import sys
+from os import urandom
+
+import iso8601
+from beancount_black.formatter import parse_date
+
+
+def make_random_id():
+    return base64.urlsafe_b64encode(urandom(24)).decode("ascii").replace("-", "")
+
+
+def get_id_maps():
+    account_ids = {}
+    txn_ids = {}
+    with open(sys.argv[1], "rt") as input_file:
+        reader = csv.DictReader(input_file)
+        for row in reader:
+            account_id = row["account_id"]
+            account_ids.setdefault(account_id, make_random_id())
+
+            txn_id = row["transaction_id"]
+            txn_ids.setdefault(txn_id, make_random_id())
+
+            pending_txn_id = row["pending_transaction_id"]
+            txn_ids.setdefault(pending_txn_id, make_random_id())
+    return account_ids, txn_ids
+
+
+def main():
+    account_ids, txn_ids = get_id_maps()
+
+    with (
+        open(sys.argv[1], "rt") as input_file,
+        open(sys.argv[2], "wt") as output_file,
+    ):
+        reader = csv.DictReader(input_file)
+        writer = csv.DictWriter(output_file, fieldnames=reader.fieldnames)
+        date_shift = random.randint(3, 10)
+        time_shift = random.uniform(60 * 60, 60 * 60 * 3)
+        writer.writeheader()
+
+        for row in reader:
+            for column, value in row.items():
+                date = None
+                dt = None
+                try:
+                    date = parse_date(value)
+                except (ValueError, TypeError):
+                    pass
+                try:
+                    dt = iso8601.parse_date(value)
+                except ValueError:
+                    pass
+                if date is not None:
+                    date += datetime.timedelta(days=date_shift)
+                    row[column] = date
+                elif dt is not None:
+                    dt += datetime.timedelta(seconds=time_shift, days=date_shift)
+                    row[column] = dt
+                elif column == "amount":
+                    row[column] = "{:.2f}".format(
+                        decimal.Decimal(value)
+                        + decimal.Decimal(random.uniform(-30, 30))
+                    )
+                elif column == "account_id":
+                    row[column] = account_ids[value]
+                elif column == "transaction_id":
+                    row[column] = txn_ids[value]
+                elif column == "pending_transaction_id":
+                    row[column] = txn_ids[value]
+            print(row)
+
+
+if __name__ == "__main__":
+    main()
